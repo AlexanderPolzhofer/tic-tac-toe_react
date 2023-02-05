c3c3442a24636e45579609006d8335d77c459f74
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-02-05 08:50:31 +0100

    calculateWinner

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Square } from "../Square/Square";
+import { calculateWinner } from "./util/calculateWinner";
 import "./Board.scss";
 
 export const Board = () => {
   const [squares, setSquares] = React.useState(new Array(9).fill(null));
   const [xIsNext, setXisNext] = React.useState(true);
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   const handleSquareClick = (i: number) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -21,23 +26,60 @@ export const Board = () => {
     setXisNext(!xIsNext);
   };
 
+  const handleDelete = () => {
+    setSquares(new Array(9).fill(null));
+    setXisNext(true);
+  };
+
   return (
-    <div className="board">
-      <div className="board__row">
-        <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleSquareClick(2)} />
+    <>
+      <div className="status">{status}</div>
+      <hr />
+      <div className="board">
+        <div className="board__row">
+          <Square
+            value={squares[0]}
+            onSquareClick={() => handleSquareClick(0)}
+          />
+          <Square
+            value={squares[1]}
+            onSquareClick={() => handleSquareClick(1)}
+          />
+          <Square
+            value={squares[2]}
+            onSquareClick={() => handleSquareClick(2)}
+          />
+        </div>
+        <div className="board__row">
+          <Square
+            value={squares[3]}
+            onSquareClick={() => handleSquareClick(3)}
+          />
+          <Square
+            value={squares[4]}
+            onSquareClick={() => handleSquareClick(4)}
+          />
+          <Square
+            value={squares[5]}
+            onSquareClick={() => handleSquareClick(5)}
+          />
+        </div>
+        <div className="board__row">
+          <Square
+            value={squares[6]}
+            onSquareClick={() => handleSquareClick(6)}
+          />
+          <Square
+            value={squares[7]}
+            onSquareClick={() => handleSquareClick(7)}
+          />
+          <Square
+            value={squares[8]}
+            onSquareClick={() => handleSquareClick(8)}
+          />
+        </div>
+        <button onClick={handleDelete}>Retry?</button>
       </div>
-      <div className="board__row">
-        <Square value={squares[3]} onSquareClick={() => handleSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleSquareClick(5)} />
-      </div>
-      <div className="board__row">
-        <Square value={squares[6]} onSquareClick={() => handleSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
-      </div>
-    </div>
+    </>
   );
 };
