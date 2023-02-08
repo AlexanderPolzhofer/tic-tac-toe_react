2ad26d3d866048722346d3e0e42f8baf43847f23
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-02-08 22:35:07 +0100

    add time travel to Game

--- src/Game.tsx
+++ src/Game.tsx
@@ -4,18 +4,39 @@ import "./Game.scss";
 
 const Game: React.FC = () => {
   const [history, setHistory] = React.useState([Array(9).fill(null)]);
-  const [xIsNext, setXisNext] = React.useState(true);
+  const [currentMove, setCurrentMove] = React.useState(0);
+  const xIsNext = currentMove % 2 === 0;
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares: any[]) => {
-    setHistory([...history, nextSquares]);
-    setXisNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   };
+
+  const jumpTo = (move: number) => {
+    setCurrentMove(move);
+  };
+
+  const moves = history.map((_, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="App">
       <Board squares={currentSquares} onPlay={handlePlay} xIsNext={xIsNext} />
+      <ol>{moves}</ol>
     </div>
   );
 };
